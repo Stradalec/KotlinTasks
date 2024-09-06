52a2c789f6d5071336b2de2b872361210df12b3b
Stradalec <pochtatipo6@gmail.com>
2024-09-06 16:42:05 +0700

    Added first part of Third task

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,35 @@
 package org.example
 
-fun main() {
+class NumberArray(private val array: IntArray) {
 
+    val sum: Int
+        get() = array.sum()
+
+    fun doubleEvens(): IntArray {
+        return array.map { if (it % 2 == 0) it * 2 else it * 1 }.toIntArray()
+    }
+
+    fun tripleOdds(): IntArray {
+        return array.map { if (it % 2 == 1) it * 3 else it * 1 }.toIntArray()
+    }
+
+    fun maxElement(): Int {
+        return array.max()
+    }
+
+    fun minElement(): Int {
+        return array.min()
+    }
+}
+
+fun main() {
+    val numbers = intArrayOf(2, 4, 3, 8, 5, 7, 6)
+    val array = NumberArray(numbers)
+
+    println("Исходный массив: ${numbers.contentToString()}")
+    println("Удвоенные четные элементы: ${array.doubleEvens().contentToString()}")
+    println("Утроенные нечетные элементы: ${array.tripleOdds().contentToString()}")
+    println("Максимальный элемент в массиве: ${array.maxElement()}")
+    println("Минимальный элемент в массиве: ${array.minElement()}")
+    println("Сумма элементов массива: ${array.sum}")
 }
