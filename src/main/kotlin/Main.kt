5390275fdad7fae04869c3bc8f30a1401defcd51
Stradalec <pochtatipo6@gmail.com>
2024-09-06 16:31:37 +0700

    Added first part of Third task

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,31 @@
 package org.example
 
-fun main() {
 
+class NumberArray(private val array: IntArray) {
+
+    fun sumOfPositiveElements(): Int {
+        var sum = 0
+        array.forEach { if (it > 0) sum += it }
+        return sum
+    }
+
+    fun productOfElements(): Long {
+        var product: Long = 1
+        array.forEach { product *= it.toLong() }
+        return product
+    }
+
+    fun averageOfElements(): Double {
+        var sum = 0
+        array.forEach { sum += it }
+        return sum.toDouble() / array.size
+    }
+}
+
+fun main() {
+    val numberArray = NumberArray(intArrayOf(3, -5, 2, 8, -1, 5))
+
+    println("Сумма положительных элементов: ${numberArray.sumOfPositiveElements()}")
+    println("Произведение элементов: ${numberArray.productOfElements()}")
+    println("Среднее арифметическое: ${numberArray.averageOfElements()}")
 }
