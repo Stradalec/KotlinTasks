0ee9aa1d7d56ccc50b92b9bcbc81250de88e4102
Stradalec <pochtatipo6@gmail.com>
2024-09-07 19:06:04 +0700

    Added second part of task eleven

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,9 @@
 package org.example
 
-fun main() {
+val concat: (Array<out String>) -> Unit = { strings ->
+    strings.sortedBy { it.length }.joinToString(" ").let(::println)
+}
 
+fun main() {
+    concat(arrayOf("угасает.", "Последствия", "лабораторной.", "быстро", "разум", "Мой"))
 }
