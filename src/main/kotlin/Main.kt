dc8acbf300339dd040b008d447e782f03ba7c641
Stradalec <pochtatipo6@gmail.com>
2024-09-07 18:47:18 +0700

    Added first part of task eleven

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,9 @@
 package org.example
 
-fun main() {
+val fahrenheit = { celsius: Int -> celsius * 9 / 5 + 32 }
 
+fun main() {
+    val celsiusTemperature = 20
+    val fahrenheitTemperature = fahrenheit(celsiusTemperature)
+    println("$celsiusTemperature °C = $fahrenheitTemperature °F")
 }
