b408093c923bfa9fb192de53ebf2f774ea135015
Stradalec <pochtatipo6@gmail.com>
2024-09-07 19:15:37 +0700

    Added fifth part of task eleven

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,57 @@
 package org.example
 
-fun main() {
+sealed class Command {
+    data class Up(val steps: Int) : Command()
+    data class Down(val steps: Int) : Command()
+    data class Left(val steps: Int) : Command()
+    data class Right(val steps: Int) : Command()
+}
 
+class Ufo(var x: Int, var y: Int) {
+    override fun toString(): String {
+        return "Тарелка Рика находится в координатах: ($x, $y)"
+    }
+
+    fun move(command: Command) {
+        when (command) {
+            is Command.Up -> {
+                y += command.steps
+                println("Тарелка Рика сместилась вверх на ${command.steps}")
+            }
+            is Command.Down -> {
+                y -= command.steps
+                println("Тарелка Рика сместилась вниз на ${command.steps}")
+            }
+            is Command.Left -> {
+                x -= command.steps
+                println("Тарелка Рика сместилась влево на ${command.steps}")
+            }
+            is Command.Right -> {
+                x += command.steps
+                println("Тарелка Рика сместилась вправо на ${command.steps}")
+            }
+        }
+    }
+}
+
+fun messageParse(encryptedMessage: String, eventUp: (Int) -> Unit, eventDown: (Int) -> Unit, eventLeft: (Int) -> Unit, eventRight: (Int) -> Unit) {
+    for (char in encryptedMessage) {
+        when (char.toUpperCase()) {
+            'A' -> eventLeft(2)
+            'D' -> eventRight(1)
+            'W' -> eventUp(1)
+            'S' -> eventDown(1)
+        }
+    }
+}
+
+fun main() {
+    val ufo = Ufo(0, 0)
+
+    messageParse("wWDaasA", { steps -> ufo.move(Command.Up(steps)) },
+        { steps -> ufo.move(Command.Down(steps)) },
+        { steps -> ufo.move(Command.Left(steps)) },
+        { steps -> ufo.move(Command.Right(steps)) })
+
+    println(ufo)
 }
