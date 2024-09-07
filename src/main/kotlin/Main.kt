7d79b13974eec36d740b5ad06f64f0c9326c58bb
Stradalec <pochtatipo6@gmail.com>
2024-09-07 19:13:28 +0700

    Added fourth part of task eleven

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,21 @@
 package org.example
 
-fun main() {
+fun messageParse(message: String, eventX: () -> Unit, eventY: () -> Unit, eventZ: () -> Unit) {
+    for (char in message) {
+        when (char.lowercaseChar()) {
+            'x' -> eventX()
+            'y' -> eventY()
+            'z' -> eventZ()
+        }
+    }
+}
 
+fun main() {
+    val inputString = "abxbbznmfkYyyxX"
+
+    messageParse(inputString,
+        { println("Обнаружен символ x") },
+        { println("Обнаружен символ y") },
+        { println("Обнаружен символ z") }
+    )
 }
