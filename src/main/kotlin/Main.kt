45119b57254ee193b433619c9afc7dc98a2c8108
Stradalec <pochtatipo6@gmail.com>
2024-09-07 18:31:09 +0700

    Added second part of task eight

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,25 @@
 package org.example
 
 fun main() {
+    val words: List<String?> = listOf("Mind is dead", "Tea is fuel " , null, "Safety of this code is full", null)
 
+    // Используя оператор if
+    println("Используя оператор if:")
+    for (word in words) {
+        if (word != null) {
+            println(word.uppercase())
+        }
+    }
+
+    // Используя оператор безопасного вызова ?
+    println("\nИспользуя оператор безопасного вызова ?:")
+    words.forEach { println(it?.uppercase()) }
+
+    // Используя функцию let
+    println("\nИспользуя функцию let:")
+    words.forEach { it?.let { word -> println(word.uppercase()) } }
+
+    // Используя Элвис-оператор ?: и функцию let
+    println("\nИспользуя Элвис-оператор ?: и функцию let:")
+    words.forEach { it?.let { word -> println(word.uppercase()) } ?: println("empty") }
 }
