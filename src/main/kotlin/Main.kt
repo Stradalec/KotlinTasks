33c47468e39caa51450d1d7046de51c94679db37
Stradalec <pochtatipo6@gmail.com>
2024-09-07 19:10:07 +0700

    Added third part of task eleven

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,14 @@
 package org.example
 
-fun main() {
+fun higherOrderFunction(message: String, convert: (String) -> String): String {
+    val convertedMessage = convert(message)
+    println(convertedMessage)
+    return convertedMessage
+}
 
+fun main() {
+    val message = "Но сейчас он был сосредоточен на работе и борьбе со сном, потому, в очередной раз отхлебнув крепкого напитка, выпрямился на стуле и окинул небольшую комнатку взглядом. Стена с доской объявлений, завешанная напутствиями в стиле «Не болтай!», металлический стул, обладающий, несомненно, важной способностью крутиться, стол, сейф с кодовым замком да огромное число приборов, стоящих напротив  входа. Обстановка не сказать что удручающая, но и явно не настраивающая на ударный труд."
+    higherOrderFunction(message) {
+        it.replace(",", " блин")
+    }
 }
